0038c0f97c332dc44d8855b0c68b1241ed5836b4
Tom Schmiedlechner <toms@sbg.at>
2017-08-19 22:35:45 +0200

    Wired context menu events, added text comparison library

--- FindSimilarWorkItems/src/app.ts
+++ FindSimilarWorkItems/src/app.ts
@@ -1,4 +1,9 @@
-﻿ /// <reference types="vss-web-extension-sdk" />
+﻿/// <reference types="vss-web-extension-sdk" />
+
+import TFS_Wit_Contracts = require("TFS/WorkItemTracking/Contracts");
+import TFS_Wit_Client = require("TFS/WorkItemTracking/RestClient");
+import TFS_Wit_Services = require("TFS/WorkItemTracking/Services");
+import StringSimilarity = require("string-similarity");
 
 class Greeter {
     element: HTMLElement;
@@ -8,7 +13,7 @@ class Greeter {
     constructor(element: HTMLElement) {
         this.element = element;
         this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
+        this.span = document.createElement("span");
         this.element.appendChild(this.span);
         this.span.innerText = new Date().toUTCString();
     }
@@ -23,6 +28,40 @@ class Greeter {
 
 }
 
-var el = document.getElementById('content');
-var greeter = new Greeter(el);
+let el = document.getElementById("content");
+let greeter = new Greeter(el);
 greeter.start();
+
+const extensionContext = VSS.getExtensionContext();
+let menuAction = {
+    execute: (actionContext) => {
+        console.log(actionContext);
+
+        TFS_Wit_Services.WorkItemFormService.getService().then(wi => {
+            (<IPromise<string>>wi.getFieldValue("System.Title")).then(title => {
+                console.log("Title: " + title);
+            });
+        });
+        TFS_Wit_Services.WorkItemFormService.getService().then(wi => {
+            (<IPromise<string>>wi.getFieldValue("Microsoft.VSTS.TCM.ReproSteps")).then(repro => {
+                console.log("Repro Steps: " + repro);
+            });
+        });
+
+        let similarity = StringSimilarity.compareTwoStrings("healed", "sealed");
+        let matches = StringSimilarity.findBestMatch("healed", ["edward", "sealed", "theatre"]);
+        console.log(similarity);
+        console.log(matches);
+    }
+};
+
+try {
+    VSS.register(`${extensionContext.publisherId}.${extensionContext.extensionId}.find-similar-workitems-menu`, menuAction);
+} catch (error) {
+    // Can be ignored - dependent on the TFS version, this may occur.
+}
+try {
+    VSS.register("find-similar-workitems-menu", menuAction);
+} catch (error) {
+    // Can be ignored - dependent on the TFS version, this may occur.
+}
