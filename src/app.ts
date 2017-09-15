1216c2594d6a299e3a0a69676ea1d7211df7b89d
Tom Schmiedlechner <toms@sbg.at>
2017-09-15 20:21:10 +0200

    Added refresh button and cleaned up code

--- src/app.ts
+++ src/app.ts
@@ -1,127 +1,14 @@
-﻿/// <reference types="vss-web-extension-sdk" />
+﻿import { Page } from "./page";
 
-import StringSimilarity = require("string-similarity");
-import Controls = require("VSS/Controls");
-import Grids = require("VSS/Controls/Grids");
-import WorkItemServices = require("TFS/WorkItemTracking/Services");
-import { WorkItemClient } from "./workitem-client";
-import { WorkItemRelation } from "TFS/WorkItemTracking/Contracts";
-
-const striptags = require("striptags");
-const decode = require("decode-html");
-
-let page = {
-    onLoaded: async (id) => {
-        const workItemClient = new WorkItemClient("Bug");
-        const currentWorkItem = await workItemClient.getCurrentWorkItemAsync(["System.Title", "Microsoft.VSTS.TCM.ReproSteps"]);
-
-        const workItems = currentWorkItem && currentWorkItem.id > 0
-            ? (await workItemClient.getAllWorkItems(["System.Title", "Microsoft.VSTS.TCM.ReproSteps"])).filter(x => x.id !== currentWorkItem.id)
-            : await workItemClient.getAllWorkItems(["System.Title", "Microsoft.VSTS.TCM.ReproSteps"]);
-
-        const workItemFormService = await WorkItemServices.WorkItemFormService.getService();
-        const relations = await workItemFormService.getWorkItemRelations();
-
-        let options: Grids.IGridOptions = {
-            width: "100%",
-            height: "100%",
-            autoSort: true,
-            sortOrder: [{ index: 2, order: "desc" }],
-            lastCellFillsRemainingContent: true,
-            source: workItems.map(w => {
-                let reproStepsOther: string = striptags(decode(w.fields["Microsoft.VSTS.TCM.ReproSteps"]));
-                let reproStepsCurrent: string = striptags(decode(currentWorkItem.fields.filter(x => x.name === "Microsoft.VSTS.TCM.ReproSteps")[0].value));
-                let titleOther: string = striptags(decode(w.fields["System.Title"]));
-                let titleCurrent: string = striptags(decode(currentWorkItem.fields.filter(x => x.name === "System.Title")[0].value));
-
-                let similarity: number = 0;
-                if (reproStepsCurrent && reproStepsOther && titleCurrent && titleOther) {
-                    let similarityTitle = StringSimilarity.compareTwoStrings(titleCurrent, titleOther);
-                    let similarityReproSteps = StringSimilarity.compareTwoStrings(reproStepsCurrent, reproStepsOther);
-                    similarity = similarityTitle * 0.7 + similarityReproSteps * 0.3;
-                }
-                return [
-                    w.id,
-                    w.fields["System.Title"],
-                    Math.floor(similarity * 1000) / 1000,
-                    w.id,
-                    reproStepsOther
-                ];
-            }),
-            columns: [
-                { text: "Id", width: 50, canMove: false, fixed: true },
-                { text: "Title", width: 200, fixed: true },
-                { text: "Similarity", width: 100, fixed: true },
-                {
-                    text: "Actions", fixed: true, getCellContents: (
-                        rowInfo,
-                        dataIndex,
-                        expandedState,
-                        level,
-                        column,
-                        indentIndex,
-                        columnOrder) => {
-
-                        const suffix = "/" + rowInfo.row.context.firstChild.innerText;
-                        let relation = relations.filter(x => x.url.indexOf(suffix, this.length - suffix.length) !== -1)[0];
-                        let linkItemIcon = relation ? "images/unlink.png" : "images/link.png";
-                        let linkItemTitle = relation ? "Remove workitem relation" : "Link workitem to current one";
-
-                        return $("<div>").addClass("grid-cell").css("padding", "5px")
-                            .append($("<span>").addClass("btn").attr("title", "Open workitem in new tab").append($("<img>").attr("src", "images/open.png"))
-                                .on("click", () => {
-                                    let id = rowInfo.row.context.firstChild.innerText;
-                                    WorkItemServices.WorkItemFormNavigationService.getService().then(workItemNavSvc => {
-                                        workItemNavSvc.openWorkItem(id, true);
-                                    });
-                                })
-                            )
-                            .append($("<span>").addClass("btn").attr("title", linkItemTitle).append($("<img>").attr("src", linkItemIcon))
-                                .on("click", (event) => {
-                                    if (relation) {
-                                        WorkItemServices.WorkItemFormService.getService().then(workitemFormService => {
-                                            workitemFormService.removeWorkItemRelations([relation]).then(() => {
-                                                relation = null;
-                                                setRelationLink(event.currentTarget, false);
-                                            });
-                                        });
-                                    }
-                                    else {
-                                        let id = +rowInfo.row.context.firstChild.innerText;
-                                        WorkItemServices.WorkItemFormService.getService().then(workitemFormService => {
-                                            workitemFormService.addWorkItemRelations([{
-                                                rel: "Related",
-                                                url: workItems.filter(x => x.id === id)[0].resourceUrl,
-                                                attributes: {}
-                                            }]).then(() => {
-                                                workitemFormService.getWorkItemRelations().then(relations => {
-                                                    relation = relations.filter(x => x.url.indexOf(suffix, this.length - suffix.length) !== -1)[0];
-                                                });
-                                                setRelationLink(event.currentTarget, true);
-                                            });
-                                        });
-                                    }
-                                })
-                            );
-                    }
-                },
-                { text: "Repro Steps", index: 4, fixed: true }
-            ]
-        };
-
-        let grid = Controls.create(Grids.Grid, $("#grid-container"), options);
-        VSS.notifyLoadSucceeded();
-    }
-};
-
-const setRelationLink = function (element: Element, relationExists: boolean): void {
-    let linkItemIcon = relationExists ? "images/unlink.png" : "images/link.png";
-    let linkItemTitle = relationExists ? "Remove workitem relation" : "Link workitem to current one";
-    element.setAttribute("title", linkItemTitle);
-    element.children[0].setAttribute("src", linkItemIcon);
-};
-
+const page = new Page();
 const extensionContext = VSS.getExtensionContext();
+
+$("#btnReload").on("click", async () => {
+    console.log("Reloading...");
+    await page.reload();
+    console.log("Loaded");
+});
+
 // >= TFS 2017
 VSS.register(`${extensionContext.publisherId}.${extensionContext.extensionId}.find-similar-workitems-form-page`, page);
 // <= TFS 2015
---
+++ src/page.ts
@@ -0,0 +1,141 @@
+/// <reference types="vss-web-extension-sdk" />
+
+import StringSimilarity = require("string-similarity");
+import Controls = require("VSS/Controls");
+import Grids = require("VSS/Controls/Grids");
+import WorkItemServices = require("TFS/WorkItemTracking/Services");
+import { WorkItemClient } from "./workitem-client";
+import { WorkItemRelation } from "TFS/WorkItemTracking/Contracts";
+import { TfsWorkItem } from "./models/tfsworkitem.model";
+
+const striptags = require("striptags");
+const decode = require("decode-html");
+
+export class Page {
+
+    private grid: Grids.Grid;
+    private workItemClient: WorkItemClient;
+    private currentWorkItem: TfsWorkItem;
+    private workItems: TfsWorkItem[];
+    private workItemFormService: WorkItemServices.IWorkItemFormService;
+    private currentRelations: WorkItemRelation[];
+
+    constructor() {
+        this.workItemClient = new WorkItemClient("Bug");
+    }
+
+    async reload() {
+        await this.loadData();
+        this.grid.setDataSource(this.workItems.map(w => this.mapWorkItems(w, this.currentWorkItem)));
+        this.grid.redraw();
+    }
+
+    async onLoaded(workItem) {
+        this.workItemFormService = await WorkItemServices.WorkItemFormService.getService();
+
+        this.grid = await this.createGrid();
+        await this.reload();
+        VSS.notifyLoadSucceeded();
+    }
+
+    private async loadData() {
+        this.currentWorkItem = await this.workItemClient.getCurrentWorkItemAsync(["System.Title", "Microsoft.VSTS.TCM.ReproSteps"]);
+        this.currentRelations = await this.workItemFormService.getWorkItemRelations();
+        this.workItems = this.currentWorkItem && this.currentWorkItem.id > 0
+            ? (await this.workItemClient.getAllWorkItems(["System.Title", "Microsoft.VSTS.TCM.ReproSteps"])).filter(x => x.id !== this.currentWorkItem.id)
+            : await this.workItemClient.getAllWorkItems(["System.Title", "Microsoft.VSTS.TCM.ReproSteps"]);
+    }
+
+    private async createGrid(): Promise<Grids.Grid> {
+        let options: Grids.IGridOptions = {
+            width: "100%",
+            height: "100%",
+            autoSort: true,
+            sortOrder: [{ index: 2, order: "desc" }],
+            lastCellFillsRemainingContent: true,
+            columns: [
+                { text: "Id", width: 50, canMove: false, fixed: true },
+                { text: "Title", width: 200, fixed: true },
+                { text: "Similarity", width: 100, fixed: true },
+                {
+                    text: "Actions", fixed: true, getCellContents: (rowInfo) => {
+                        return $("<div>").addClass("grid-cell").css("padding", "5px")
+                            .append(this.createViewButton(rowInfo))
+                            .append(this.createLinkButton(rowInfo));
+                    }
+                },
+                { text: "Repro Steps", index: 4, fixed: true }
+            ]
+        };
+
+        return Controls.create(Grids.Grid, $("#grid-container"), options);
+    }
+
+    private createViewButton(rowInfo: any): any {
+        return $("<span>").addClass("btn").attr("title", "Open workitem in new tab").append($("<img>").attr("src", "images/open.png"))
+            .on("click", () => {
+                let id = rowInfo.row.context.firstChild.innerText;
+                WorkItemServices.WorkItemFormNavigationService.getService().then(workItemNavSvc => {
+                    workItemNavSvc.openWorkItem(id, true);
+                });
+            });
+    }
+
+    private createLinkButton(rowInfo: any): any {
+        const suffix = "/" + rowInfo.row.context.firstChild.innerText;
+        let relation = this.currentRelations.filter(x => x.url.indexOf(suffix, x.url.length - suffix.length) !== -1)[0];
+        let linkItemIcon = relation ? "images/unlink.png" : "images/link.png";
+        let linkItemTitle = relation ? "Remove workitem relation" : "Link workitem to current one";
+        return $("<span>").addClass("btn").attr("title", linkItemTitle).append($("<img>").attr("src", linkItemIcon))
+            .on("click", (event) => {
+                if (relation) {
+                    this.workItemFormService.removeWorkItemRelations([relation]).then(() => {
+                        relation = null;
+                        this.setRelationLink(event.currentTarget, false);
+                    });
+                }
+                else {
+                    let id = +rowInfo.row.context.firstChild.innerText;
+
+                    this.workItemFormService.addWorkItemRelations([{
+                        rel: "Related",
+                        url: this.workItems.filter(x => x.id === id)[0].resourceUrl,
+                        attributes: {}
+                    }]).then(() => {
+                        this.workItemFormService.getWorkItemRelations().then(relations => {
+                            relation = relations.filter(x => x.url.indexOf(suffix, x.url.length - suffix.length) !== -1)[0];
+                        });
+                        this.setRelationLink(event.currentTarget, true);
+                    });
+                }
+            });
+    }
+
+    private mapWorkItems(workItem: TfsWorkItem, currentWorkItem: TfsWorkItem) {
+        let reproStepsOther: string = striptags(decode(workItem.fields["Microsoft.VSTS.TCM.ReproSteps"]));
+        let reproStepsCurrent: string = striptags(decode(currentWorkItem.fields.filter(x => x.name === "Microsoft.VSTS.TCM.ReproSteps")[0].value));
+        let titleOther: string = striptags(decode(workItem.fields["System.Title"]));
+        let titleCurrent: string = striptags(decode(currentWorkItem.fields.filter(x => x.name === "System.Title")[0].value));
+
+        let similarity: number = 0;
+        if (reproStepsCurrent && reproStepsOther && titleCurrent && titleOther) {
+            let similarityTitle = StringSimilarity.compareTwoStrings(titleCurrent, titleOther);
+            let similarityReproSteps = StringSimilarity.compareTwoStrings(reproStepsCurrent, reproStepsOther);
+            similarity = similarityTitle * 0.7 + similarityReproSteps * 0.3;
+        }
+        return [
+            workItem.id,
+            workItem.fields["System.Title"],
+            Math.floor(similarity * 1000) / 1000,
+            workItem.id,
+            reproStepsOther
+        ];
+    }
+
+    private setRelationLink(element: Element, relationExists: boolean): void {
+        let linkItemIcon = relationExists ? "images/unlink.png" : "images/link.png";
+        let linkItemTitle = relationExists ? "Remove workitem relation" : "Link workitem to current one";
+        element.setAttribute("title", linkItemTitle);
+        element.children[0].setAttribute("src", linkItemIcon);
+    }
+}
