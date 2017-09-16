6389fdd823a486eb91462de80628899520c03b9e
Stefan Kert <stefan.kert@sbg.at>
2017-09-16 17:59:01 +0200

    Added helper method for getting field for fieldname

--- src/models/tfsworkitem.model.ts
+++ src/models/tfsworkitem.model.ts
@@ -3,4 +3,8 @@ import { Field } from "./field.model";
 export class TfsWorkItem {
 
     constructor(public id: number, public fields: Field[], public resourceUrl?: string) { }
+
+    getFieldValue(fieldName: string): string {
+        return this.fields.filter(x => x.name === fieldName)[0].value;
+    }
 }
